85f1dcd40772ceddd2bbe4278f9e01cec9bea5c3
riahim <mihairad@umich.edu>
2016-01-26 00:10:26 -0500

    Update list.c

--- list.c
+++ list.c
@@ -10,6 +10,60 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
+	
+	list_t* temp = head;
+	list_t *left;
+	list_t *right;
+	
+	if(new_element -> index <= head->index)
+	{
+		new_element->next = head;
+		head = new_element;
+	
+	}
+	
+	/*printf("%u\n", *head);
+	temp = temp->next;
+	printf("%s", "NEXT VALUE\n");
+	printf("%u\n", *temp);
+	temp = temp->next;
+	printf("%s", "SECOND VALUE\n");
+	printf("%u\n", *temp);*/
+	
+	else
+	{
+	
+		//printf("%u\n", *new_element);
+		//printf("%u\n", temp->index);
+		
+		while((new_element -> index > temp->index) && (temp->next !=NULL))
+		{
+			left = temp;
+			right = temp->next;
+			new_element->next = temp; 
+			
+			temp = temp->next;
+			
+			/*printf("%s", "LEFT\n");
+			printf("%u\n", *left);
+			printf("%s", "Temp\n");
+			printf("%u\n", *temp);
+			printf("%s", "new element\n");
+			printf("%u\n", *new_element);*/
+					
+		}
+		if(temp->next == NULL && temp->index<new_element->index)
+		{
+			temp->next = new_element;
+			new_element->next = NULL;
+			
+		}
+		else
+		{
+		left->next = new_element;
+		new_element->next = right;
+		}
+	}
 
 	return head;
 }
@@ -18,7 +72,17 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
-
+	
+	list_t* prev = NULL;
+	list_t* next;
+	while(head != NULL)
+	{
+		next = head->next;
+		head->next = prev;
+		prev = head;
+		head = next;
+	}
+	head = prev;
+	
 	return head;
 }
-
